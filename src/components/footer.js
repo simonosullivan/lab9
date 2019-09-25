85ec5d573c43a94db4dbd68bd71cade4f53799a6
Martin Kenirons <martin.kenirons@gmit.ie>
2019-09-25 10:40:18 +0100

    first-final commit

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My Footer in another component</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
